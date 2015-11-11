357dd7ef6ef549de8ace63fe8574342a6313ca0b
kiselia <asen_kiselov@abv.bg>
2015-11-11 17:38:19 +0200

    patch?
    launcher add

--- PileGame/src/game/Launcher.java
+++ PileGame/src/game/Launcher.java
@@ -3,38 +3,55 @@ package game;
 import display.Display;
 import gfx.ImageLoader;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Launcher {
+    private static Graphics g;
+
     public static void main(String[] args) {
+        JFrame window = new JFrame("Launcher");
+        window.setVisible(true);
+        window.setSize(400,400);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setFocusable(true);
+        window.setLocationRelativeTo(null);
 
-        //launcher frame
-        JFrame f = new JFrame("Launcher");
-        f.setMaximumSize(new Dimension(360, 360));
-        f.setPreferredSize(new Dimension(360, 360));
-        f.setMinimumSize(new Dimension(360, 360));
-        f.setVisible(true);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setResizable(false);
-        f.setFocusable(true);
-        f.setLocationRelativeTo(null);
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        window.add(panel);
+        //Beckground ??????????
+        panel.setBackground(Color.white);
 
 
-        JPanel p = new JPanel();
-        p.setBackground(Color.BLUE);
-        f.add(p);
 
-        JButton startButton = new JButton("Play Game");
-        startButton.setPreferredSize(new Dimension(100,40));
 
+        JButton startButton = new JButton("START");
+        startButton.setBounds(150,70,100,50);
+        panel.add(startButton);
         startButton.addActionListener(new Action());
 
-        p.add(startButton);
 
-        f.pack();
+        JButton exitButton = new JButton("EXIT");
+        exitButton.setBounds(150,280,100,50);
+        panel.add(exitButton);
+        //fincion needed
+        JButton controlsButton = new JButton("CONTROLS");
+        controlsButton.setBounds(150,140,100,50);
+        panel.add(controlsButton);
+        // funcion needed
+        JButton creditsButton = new JButton("CREDITS");
+        creditsButton.setBounds(150,210,100,50);
+        panel.add(creditsButton);
+        // funcion needed
+
     }
 
         static class Action implements ActionListener{
